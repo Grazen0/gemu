44dbfb063dd5c076e3d5f1fd07e54d2f665b7651
José Daniel Grayson <josedanielgrayson@proton.me>
2025-06-03 23:09:19 -0500

    test: add some cpu and num tests

--- tests/common/test_num.c
+++ tests/common/test_num.c
@@ -1,1 +1,26 @@
-void test_concat_u16(void) {}
+#include <stdint.h>
+#include "common/num.h"
+#include "unity.h"
+
+void test_concat_u16(void) {
+    TEST_ASSERT_EQUAL_HEX(0x1234, concat_u16(0x12, 0x34));
+    TEST_ASSERT_EQUAL_HEX(0x2618, concat_u16(0x26, 0x18));
+    TEST_ASSERT_EQUAL_HEX(0x0000, concat_u16(0x00, 0x00));
+    TEST_ASSERT_EQUAL_HEX(0xFF00, concat_u16(0xFF, 0x00));
+    TEST_ASSERT_EQUAL_HEX(0x00FF, concat_u16(0x00, 0xFF));
+}
+
+void test_set_bits(void) {
+    uint8_t bits = 0;
+
+    set_bits(&bits, 0x23, true);
+    TEST_ASSERT_EQUAL_HEX(0x23, bits);
+    set_bits(&bits, 0x21, false);
+    TEST_ASSERT_EQUAL_HEX(0x02, bits);
+    set_bits(&bits, 0x8C, true);
+    TEST_ASSERT_EQUAL_HEX(0x8E, bits);
+    set_bits(&bits, 0xFF, false);
+    TEST_ASSERT_EQUAL_HEX(0x00, bits);
+    set_bits(&bits, 0xFF, true);
+    TEST_ASSERT_EQUAL_HEX(0xFF, bits);
+}
--- tests/core/test_cpu.c
+++ tests/core/test_cpu.c
@@ -1,8 +1,33 @@
+#include <stdint.h>
+#include <string.h>
 #include "core/cpu.h"
 #include "unity.h"
 
+static Cpu cpu;
+static uint8_t mem_data[0x10000];
+static Memory mock_memory;
+
+static uint8_t read_mock_mem(const void* ctx, const uint16_t addr) {
+    const uint8_t* const restrict memory = ctx;
+    return memory[addr];
+}
+
+static void write_mock_mem(void* ctx, const uint16_t addr, const uint8_t value) {
+    uint8_t* const restrict memory = ctx;
+    memory[addr] = value;
+}
+
+void setUp(void) {
+    cpu = Cpu_new();
+    mock_memory = (Memory){
+        .ctx = mem_data,
+        .read = read_mock_mem,
+        .write = write_mock_mem,
+    };
+    memset(mem_data, 0, sizeof(mem_data));
+}
+
 void test_cpu_new(void) {
-    Cpu cpu = Cpu_new();
     TEST_ASSERT_EQUAL(cpu.b, 0);
     TEST_ASSERT_EQUAL(cpu.c, 0);
     TEST_ASSERT_EQUAL(cpu.d, 0);
@@ -15,4 +40,10 @@ void test_cpu_new(void) {
     TEST_ASSERT_EQUAL(cpu.pc, 0);
     TEST_ASSERT_EQUAL(cpu.halted, false);
     TEST_ASSERT_EQUAL(cpu.ime, true);
+    TEST_ASSERT_EQUAL(cpu.cycle_count, 0);
+}
+
+void test_cpu_write_mem(void) {
+    Cpu_write_mem(&cpu, &mock_memory, 0x0000, 0x42);
+    TEST_ASSERT_EQUAL_HEX(0x42, mem_data[0x0000]);
 }

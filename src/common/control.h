b54d928793c77f4ff1075d61f6c45244edcac86d
José Daniel Grayson <josedanielgrayson@proton.me>
2025-07-23 01:18:12 -0500

    style: format files

--- src/common/control.h
+++ src/common/control.h
@@ -1,25 +1,25 @@
 #ifndef COMMON_CONTROL_H
 #define COMMON_CONTROL_H
 
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 
-#define BAIL(...)                                              \
-    do {                                                       \
-        fprintf(stderr, "BAIL (%s:%d): ", __FILE__, __LINE__); \
-        fprintf(stderr, __VA_ARGS__);                          \
-        fputc('\n', stderr);                                   \
-        exit(1);                                               \
+#define BAIL(...)                                                                                  \
+    do {                                                                                           \
+        fprintf(stderr, "BAIL (%s:%d): ", __FILE__, __LINE__);                                     \
+        fprintf(stderr, __VA_ARGS__);                                                              \
+        fputc('\n', stderr);                                                                       \
+        exit(1);                                                                                   \
     } while (0);
 
-#define BAIL_IF(cond, ...)                                                         \
-    do {                                                                           \
-        if (cond) {                                                                \
-            fprintf(stderr, "BAIL_IF('%s') (%s:%d): ", #cond, __FILE__, __LINE__); \
-            fprintf(stderr, __VA_ARGS__);                                          \
-            fputc('\n', stderr);                                                   \
-            exit(1);                                                               \
-        }                                                                          \
+#define BAIL_IF(cond, ...)                                                                         \
+    do {                                                                                           \
+        if (cond) {                                                                                \
+            fprintf(stderr, "BAIL_IF('%s') (%s:%d): ", #cond, __FILE__, __LINE__);                 \
+            fprintf(stderr, __VA_ARGS__);                                                          \
+            fputc('\n', stderr);                                                                   \
+            exit(1);                                                                               \
+        }                                                                                          \
     } while (0);
 
 #endif

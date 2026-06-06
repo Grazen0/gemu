fb1fdc5d06137f04e26c8b4dc72bae2cc91b51ca
José Daniel Grayson <josedanielgrayson@proton.me>
2026-06-06 03:00:17 -0500

    style: remove some trivial consts

--- src/num.h
+++ src/num.h
@@ -3,12 +3,12 @@
 
 #include "stdinc.h"
 
-[[nodiscard]] static inline u16 concat_u16(const u8 hi, const u8 lo)
+[[nodiscard]] static inline u16 concat_u16(u8 hi, u8 lo)
 {
     return ((u16)hi << 8) | (u16)lo;
 }
 
-static inline void set_bits(u8 *const dest, const u8 mask, const bool value)
+static inline void set_bits(u8 *dest, u8 mask, bool value)
 {
     if (value)
         *dest |= mask;

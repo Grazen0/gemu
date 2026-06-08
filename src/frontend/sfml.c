78eb3b5f387f7ece6c252fbf9aca13500107c66c
José Daniel Grayson <josedanielgrayson@proton.me>
2026-06-08 12:30:54 -0500

    refactor(sfml): clean up some sfml code

--- src/frontend/sfml.c
+++ src/frontend/sfml.c
@@ -1,10 +1,10 @@
 #include "frontend/sfml.h"
 #include "common.h"
 #include "game_boy.h"
+#include "log.h"
 #include "scheduler.h"
 #include "stdinc.h"
 #include <CSFML/Graphics.h>
-#include <CSFML/Window.h>
 #include <assert.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -15,30 +15,23 @@
 #define RGBA_ALLOC_SIZE 4
 #define TO_COLOR(rgb) {(rgb)[0], (rgb)[1], (rgb)[2], 255}
 
-static void update_pixels(const GameBoy *gb, sfTexture *tex, u8 *rgb_buf)
+static void update_pixels(const GameBoy *gb, sfColor pixel_buf[])
 {
     static const sfColor PALETTE[] = {
-        TO_COLOR(PALETTE_RGB[0]), TO_COLOR(PALETTE_RGB[1]),
-        TO_COLOR(PALETTE_RGB[2]), TO_COLOR(PALETTE_RGB[3])};
+        TO_COLOR(PALETTE_RGB[0]),
+        TO_COLOR(PALETTE_RGB[1]),
+        TO_COLOR(PALETTE_RGB[2]),
+        TO_COLOR(PALETTE_RGB[3]),
+    };
     static_assert(ARRAY_LEN(PALETTE) == PALETTE_RGB_LEN);
 
     for (size_t p = 0; p < (size_t)GB_LCD_WIDTH * GB_LCD_HEIGHT; ++p) {
-        const size_t y = p / GB_LCD_WIDTH;
-        const size_t x = p % GB_LCD_WIDTH;
+        size_t y = p / GB_LCD_WIDTH;
+        size_t x = p % GB_LCD_WIDTH;
 
         u8 color = gb->scanout_buf[y][x];
-        const sfColor out_col = PALETTE[color];
-
-        const size_t dst = p * 4;
-        rgb_buf[dst] = out_col.r;
-        rgb_buf[dst + 1] = out_col.g;
-        rgb_buf[dst + 2] = out_col.b;
-        rgb_buf[dst + 3] = out_col.a;
+        pixel_buf[p] = PALETTE[color];
     }
-
-    sfTexture_updateFromPixels(tex, rgb_buf,
-                               (sfVector2u){GB_LCD_WIDTH, GB_LCD_HEIGHT},
-                               (sfVector2u){0, 0});
 }
 
 static void update_screen(sfSprite *spr, sfRenderWindow *wnd, sfView *view)
@@ -88,63 +81,87 @@ static bool *map_joypad_btn(JoypadButtons *joypad, sfKeyCode key)
     }
 }
 
+static void handle_event(sfEvent *event, GameBoy *gb, sfRenderWindow *window,
+                         bool *quit)
+{
+
+    if (event->type == sfEvtClosed) {
+        sfRenderWindow_close(window);
+        *quit = true;
+        return;
+    }
+
+    if (event->type == sfEvtKeyPressed || event->type == sfEvtKeyReleased) {
+        bool *joypad_btn = map_joypad_btn(&gb->btns, event->key.code);
+
+        if (joypad_btn != nullptr)
+            *joypad_btn = event->type == sfEvtKeyPressed;
+    }
+}
+
 static int run(GameBoy *gb)
 {
     sfVideoMode vid_mode = {
         {WINDOW_INIT_WIDTH, WINDOW_INIT_HEIGHT},
-        32
+        32,
     };
+
+    log_info("Creating window");
     sfRenderWindow *window = sfRenderWindow_create(
-        vid_mode, "gemu", sfTitlebar | sfClose | sfResize, sfWindowed, nullptr);
+        vid_mode, "gemu", sfDefaultStyle, sfWindowed, nullptr);
+    if (window == nullptr)
+        BAIL("Could not create window.");
+
     sfRenderWindow_setFramerateLimit(window, 60);
 
+    log_info("Creating view");
     sfView *view = sfView_create();
+    if (view == nullptr)
+        BAIL("Could not create view.");
 
-    sfVector2u tex_size = {GB_LCD_WIDTH, GB_LCD_HEIGHT};
-    sfTexture *texture = sfTexture_create(tex_size);
+    log_info("Creating texture");
+    sfVector2u texture_size = {GB_LCD_WIDTH, GB_LCD_HEIGHT};
+    sfTexture *texture = sfTexture_create(texture_size);
+    if (texture == nullptr)
+        BAIL("Could not create texture.");
 
     sfSprite *gb_spr = sfSprite_create(texture);
-    u8 pixel_buffer[(size_t)GB_LCD_WIDTH * GB_LCD_HEIGHT * sizeof(u8) *
-                    RGBA_ALLOC_SIZE];
+    if (gb_spr == nullptr)
+        BAIL("Could not create texture.");
 
-    sfClock *clock = sfClock_create();
+    sfColor *pixel_buf =
+        calloc((size_t)GB_LCD_WIDTH * GB_LCD_HEIGHT, sizeof(*pixel_buf));
+    assert(pixel_buf != nullptr);
+
     Scheduler sched = sched_init();
 
-    sfEvent cur_even;
-    bool exiting = false;
-    while (sfRenderWindow_isOpen(window) && !exiting) {
+    bool quit = false;
 
-        while (sfRenderWindow_pollEvent(window, &cur_even)) {
-            if (cur_even.type == sfEvtClosed) {
-                sfRenderWindow_close(window);
-                exiting = true;
-            } else if (cur_even.type == sfEvtKeyPressed ||
-                       cur_even.type == sfEvtKeyReleased) {
-                bool *joypad_btn = map_joypad_btn(&gb->btns, cur_even.key.code);
+    sfClock *clock = sfClock_create();
+    assert(clock != nullptr);
 
-                if (joypad_btn != nullptr) {
-                    *joypad_btn = cur_even.type == sfEvtKeyPressed;
-                    break;
-                }
-            }
-        }
+    while (sfRenderWindow_isOpen(window) && !quit) {
+        sfEvent event = {};
+        while (sfRenderWindow_pollEvent(window, &event))
+            handle_event(&event, gb, window, &quit);
 
-        if (exiting)
-            break;
-
-        sfTime elapsed = sfClock_getElapsedTime(clock);
-        long double cur_time =
-            (long double)sfTime_asMicroseconds(elapsed) / 1000000.0L;
+        long double cur_time = sfTime_asSeconds(sfClock_getElapsedTime(clock));
         u64 cur_time_clk = (u64)(cur_time * GB_CLK_FREQ_HZ);
 
         sched_dispatch_until(&sched, gb, cur_time_clk);
-        update_pixels(gb, texture, pixel_buffer);
+        update_pixels(gb, pixel_buf);
+        sfTexture_updateFromPixels(texture, (u8 *)pixel_buf, texture_size,
+                                   (sfVector2u){0, 0});
         update_screen(gb_spr, window, view);
     }
 
-    sfView_destroy(view);
+    log_info("Cleaning up SFML");
+    sfClock_destroy(clock);
+    sched_deinit(&sched);
+    free(pixel_buf);
     sfSprite_destroy(gb_spr);
     sfTexture_destroy(texture);
+    sfView_destroy(view);
     sfRenderWindow_destroy(window);
     return EXIT_SUCCESS;
 }
